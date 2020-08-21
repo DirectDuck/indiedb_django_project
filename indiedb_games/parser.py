d0a17c1831611425ff0fbb9393fd4aef45b766d8
dduck <famgui14@gmail.com>
2020-08-21 09:46:57 +0300

    redesigned parser

--- indiedb_games/parser.py
+++ indiedb_games/parser.py
@@ -5,39 +5,48 @@ from .models import Game
 
 link = r'https://www.indiedb.com/games/page/1?sort=id-desc#gamesbrowse'
 
-def _scrap_page(soup: BeautifulSoup) -> dict:
+def parse_status_and_genre(game_container):
+    current_span = game_container.find('span', class_='subheading')
+
+    status = current_span.time.text
+
+    time_tag_index = str(current_span).index('</time>')
+    end_of_span = str(current_span).index('</span>')
+    genre = str(current_span)[time_tag_index + 7:end_of_span].strip()
+    return status, genre
+
+
+def _scrap_new_games(soup: BeautifulSoup) -> dict:
     content = []
     titles = soup.findAll('h4')
-    information = soup.findAll('div', class_='content')[1:]
-    
+    game_containers = soup.findAll('div', class_='content')[1:]
+
+    all_game_objects_in_db = Game.objects.all()
+
     for i in range(len(titles)):
-        current_span = information[i].find('span', class_='subheading')
-        
-        status = current_span.time.text
-        
-        time_tag_index = str(current_span).index('</time>')
-        end_of_span = str(current_span).index('</span>')
-        genre = str(current_span)[time_tag_index+7:end_of_span].strip()
-        
-        content.append({'title': titles[i].text, 'status': status, 'genre': genre})
-    
+        title = titles[i].text
+        try:
+            all_game_objects_in_db.get(title=title)
+            break
+        except:
+            status, genre = parse_status_and_genre(game_containers[i])
+            content.append({'title': title, 'status': status, 'genre': genre})
+
     return content
 
-def _add_game_to_db(game):
+
+def _add_game_to_db(game: dict) -> None:
     new_db_object = Game(
         title=game['title'],
         genre=game['genre'],
         status=game['status'])
     new_db_object.save()
 
+
 def update_db() -> None:
     source = requests.get(link).text
     soup = BeautifulSoup(source, 'lxml')
-    scrapped_games = _scrap_page(soup)
-    all_game_objects_in_db = Game.objects.all()
-    for game in scrapped_games:
-        try:
-            all_game_objects_in_db.get(title=game['title'])
-        except:
-            _add_game_to_db(game)
-
+    scrapped_games = _scrap_new_games(soup)
+    
+    for game in reversed(scrapped_games):
+        _add_game_to_db(game)
